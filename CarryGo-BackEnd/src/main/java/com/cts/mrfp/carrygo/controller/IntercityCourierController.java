5e610ed2570b3ce77622f9dffd3da803d1b85779
koushik1519 <154420105+KovelaHemaKoushikeswarReddy@users.noreply.github.com>
2026-05-04 23:59:09 +0530

    Update IntercityCourierController.java

--- CarryGo-BackEnd/src/main/java/com/cts/mrfp/carrygo/controller/IntercityCourierController.java
+++ CarryGo-BackEnd/src/main/java/com/cts/mrfp/carrygo/controller/IntercityCourierController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("https://carrygo-production.up.railway.app/api/intercity")
+@RequestMapping("/api/intercity")
 @CrossOrigin(origins = "*")
 public class IntercityCourierController {
 
